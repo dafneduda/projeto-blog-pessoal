ae29c9d4df582cda260ede00077c97fcfcb31e7f
Dafne Duda <dafne.leslie@gmail.com>
2024-03-23 17:49:17 -0300

    Testes JUnit

--- src/main/java/com/generation/blogPessoal/model/Usuario.java
+++ src/main/java/com/generation/blogPessoal/model/Usuario.java
@@ -43,8 +43,6 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
-	/* Insira os Getters and Setters */
-
 	public Long getId() {
 		return this.id;
 	}
@@ -91,6 +89,18 @@ public class Usuario {
 
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
+	}
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+
+	public Usuario() {
+
 	}
 
 }
